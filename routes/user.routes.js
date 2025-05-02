6a9876ccfcdddfab4da4c389396a61d4d28c9294
atul pandey <apk200209@gmail.com>
2025-05-02 17:50:21 +0530

    commit

---
+++ routes/user.routes.js
@@ -0,0 +1,26 @@
+const express  = require("express");
+const User = require("../model/user.model.js");
+
+// Destructure the controllers from the controllers file
+const { postRoute, getRoute, getID, putID, deleteID } = require("../controllers/user.controllers.js");
+
+// Router helps in managing the routes for a particular category
+const router = express.Router();
+
+// Post users route
+router.post("/", postRoute);
+
+// Get users route
+router.get("/", getRoute);
+
+// Dynamic routing
+// Quering and we do it by query parameters
+router.get("/:id", getID);
+
+// Updating route
+router.put("/:id", putID);
+
+// Deleting route
+router.delete("/:id", deleteID);
+
+module.exports = router;
